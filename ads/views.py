15c42556cd1390e1d78952bbf9b374da2df6e4aa
rshri <rshriharripriya19@gmail.com>
2022-01-31 18:38:26 +0530

    ghub

--- ads/views.py
+++ ads/views.py
@@ -18,7 +18,7 @@ from django.db.models import Q
 
 class AdListView(OwnerListView):
     model = Ad
-    template_name = "ad_list.html"
+    template_name = "index.html"
 
     def get(self, request):
         strval = request.GET.get("search", False)
